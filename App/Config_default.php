e424b4699c4113c950d1da803269fb6d59c753a3
Karol Sołek <karol.solek.programista@gmail.com>
2019-07-24 02:52:58 +0200

    Config.php_edit

--- App/Config_default.php
+++ App/Config_default.php
@@ -6,12 +6,12 @@ namespace App;
  * PHP version 7.0
  */
 class Config {
-	const DB_HOST = 'localhost';
+	const DB_HOST = '';
 	const DB_NAME = 'personal_budget';
-	const DB_USER = 'root';
+	const DB_USER = '';
 	const DB_PASSWORD = '';
 	
 	const SHOW_ERRORS = false;
 	
-	const SECRET_KEY = 'v2EXvwymjfi51GwZ3GZdSAwUu409dnv8';
+	const SECRET_KEY = '';
 }
