81a3678956abf65a9e323369ad8db4d3d76c46b6
Karol Sołek <karol.solek.programista@gmail.com>
2019-10-03 23:51:55 +0200

    View Income: html+css+js redesign

---
+++ public/js/income.js
@@ -0,0 +1,29 @@
+const $form = $('#income_form');
+const $submitBtn = $("#submitBtn");
+const $info = $("#info_ribbon");
+$form.on("submit", function(e) {
+    e.preventDefault();
+    $submitBtn.prop('disabled', true);
+	$.ajax({
+        url: '/income/add-income-ajax',
+        method : "POST",
+        dataType : "json",
+        data: $(this).serialize()
+    }).done(function(response) {
+		$(".alert-warning").hide();
+		if(response.success) {
+			$info.removeClass('alert-danger');
+			$info.addClass('alert-success');
+			$info.html(response.message);
+			$(".alert-success").show();
+			$submitBtn.prop('disabled', false);
+		} else {
+			location.reload(true);
+		}
+    }).fail(function() {
+		$info.removeClass('alert-success');
+		$info.addClass('alert-danger');
+		$info.html('Błąd połączenia z bazą danych.');
+		$submitBtn.prop('disabled', false);
+	});
+});
