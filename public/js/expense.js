a18592323e461258d1ae420348ea0624676948f2
Karol Sołek <karol.solek.programista@gmail.com>
2019-10-07 01:11:53 +0200

    View Expense limit info added

--- public/js/expense.js
+++ public/js/expense.js
@@ -8,47 +8,21 @@ $(document).ready(function() {
 	const $category = $("input[name='expense_category']");
 	const $dateInput = $('#expense_date');
 	
-	function infoText(name, limit_value, beforeSum, afterSum) {
-		let message = '<div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;">'+name+'</div><div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;">Limit: '+limit_value+'</div><div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;"> Teraz: '+beforeSum+'</div><div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;"> Łącznie: '+afterSum+'</div>';
-		return message;
+	function alertInfo(text) {
+		$info.removeClass('alert-success');
+		$info.addClass('alert-danger');
+		$info.html(text);
 	}
 	
-	function updateInfo($valueInput, $checked) {
-		let amount = $valueInput.val();
-		let category_id = $checked.val();
-		let limit_value = $checked.attr("data-limit-value");
-		let date = $dateInput.val();
-		$.ajax({
-			url: '/expense/get-perioded-sum-ajax',
-			method : "POST",
-			dataType : "json",
-			data: {
-				"cat_id" : category_id,
-				"date" : date,
-				"amount" : amount
-			}
-		}).done(function(response) {
-			console.log(response.success);
-			if (response.success) {
-				if(response.limit_reached) {
-					$info.removeClass('alert-success');
-					$info.addClass('alert-danger');
-				} else {
-					$info.removeClass('alert-danger');
-					$info.addClass('alert-success');
-				}
-				let name = $checked.attr("data-name");
-				$info.html(infoText(name,limit_value,response.beforeSum,response.afterSum));
-			} else {
-				$info.removeClass('alert-success');
-				$info.addClass('alert-danger');
-				$info.html(response.error);
-			}
-		}).fail(function() {
-			$info.removeClass('alert-success');
-			$info.addClass('alert-danger');
-			$info.html('Błąd połączenia z bazą danych.');
-		});
+	function successInfo(text) {
+		$info.removeClass('alert-danger');
+		$info.addClass('alert-success');
+		$info.html(text);
+	}
+	
+	function infoText(name, limit_value, beforeSum, afterSum) {
+		let message = '<div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;">'+name+'</div><div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;">Limit: '+limit_value+'</div><div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;"> W miesiącu: '+beforeSum+'</div><div class="col-md-3 col-sm-6 col-12" style="float: left; text-align: left;"> Łącznie: '+afterSum+'</div>';
+		return message;
 	}
 	
 	function showEstimation($valueInput) {
@@ -61,11 +35,92 @@ $(document).ready(function() {
 		}
 	}
 	
+	function validateValueWithInfo(input_value) {
+		if (input_value != '') {
+			if (input_value.length > 9) {
+				alertInfo("Za długa wartości wydatku.");
+				$expenseValue.val("");
+				return false;
+			} else if ((input_value <= 0) || (input_value.match(/^0+[,.]0+$/) != null)){
+				alertInfo(input_value+' (Wartość wydatku musi być większa od zera.)');
+				return false;
+			} else if (input_value >= 1000000){
+				alertInfo(input_value+' (Wartość wydatku musi być mniejsza niż milion.)');
+				return false;
+			} else if ((input_value.match(/^\d{1,6}[,.]?\d{0,2}$/) != null) 
+							&& (input_value.match(/^\d{1,6}[,.]$/) == null)) {
+				return true;
+			}  else {
+				alertInfo(input_value+'<br/> (Niepoprawny format wartości wydatku.)');
+				return false;
+			}
+		}
+		alertInfo("Nie podano wartości wydatku.");
+		return false;
+	}
+	
+	function validateValue(input_value) {
+		console.log('validateValue');
+		if (input_value===0) return true;
+		if (input_value != '') {
+			if (input_value.length > 9) {
+				return false;
+			} else if ((input_value < 0) || (input_value.match(/^0+[,.]0+$/) != null)){
+				return false;
+			} else if (input_value >= 1000000){
+				return false;
+			} else if ((input_value.match(/^\d{1,6}[,.]?\d{0,2}$/) != null) 
+							&& (input_value.match(/^\d{1,6}[,.]$/) == null)) {
+				return true;
+			}  else {
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	function ajaxGetLimit() {
+		;
+	}
+	
+	function updateInfo($valueInput, $checked) {
+		let amount = $valueInput.val();
+		if (!amount) amount=0;
+		console.log("val: "+amount);
+		if(validateValue(amount)) {
+			let category_id = $checked.val();
+			let limit_value = $checked.attr("data-limit-value");
+			let date = $dateInput.val();
+			$.ajax({
+				url: '/expense/get-perioded-sum-ajax',
+				method : "POST",
+				dataType : "json",
+				data: {
+					"cat_id" : category_id,
+					"date" : date,
+					"amount" : amount
+				}
+			}).done(function(response) {
+				if (response.success) {
+					let name = $checked.attr("data-name");
+					let text = infoText(name,limit_value,response.beforeSum,response.afterSum);
+					response.limit_reached ? alertInfo(text) : successInfo(text);
+				} else {
+					alertInfo(response.error);
+				}
+			}).fail(function() {
+				alertInfo('Błąd połączenia z bazą danych.');
+			});
+		} else {
+			$info.empty();
+		}
+	}
+
 	/* Add expense (AJAX request) */
 	$form.on("submit", function(e) {
 		e.preventDefault();
 		$submitBtn.prop('disabled', true);
-		if($expenseValue.val()) {
+		if(validateValueWithInfo($expenseValue.val())) {
 			$.ajax({
 				url: '/expense/add-expense-ajax',
 				method : "POST",
@@ -74,35 +129,31 @@ $(document).ready(function() {
 			}).done(function(response) {
 				$(".alert-warning").hide();
 				if(response.success) {
-					$info.removeClass('alert-danger');
-					$info.addClass('alert-success');
-					$info.html(response.message);
+					successInfo(response.message);
 					$(".alert-success").show();
-					$expenseValue.empty();
-					$expenseValue.focus();
 				} else {
 					location.reload(true);
 				}
 			}).fail(function() {
-				$info.removeClass('alert-success');
-				$info.addClass('alert-danger');
-				$info.html('Błąd połączenia z bazą danych.');
+				alertInfo('Błąd połączenia z bazą danych.');
 			});
 		}
+		$expenseValue.val("");
 		setTimeout(function() {$submitBtn.prop('disabled', false);}, 1000);
+		$expenseValue.focus();
 	});
-	
-	/* Prototype: show ribbon with data for limit request */
-	
 	
 	$expenseValue.keyup(function() {
 		$(this).prop('disabled', true);
 		($(this).val().length>9) ? $(this).val($(this).val().slice(0, -1)) : false;
-		$(this).val() ? showEstimation($(this)) : $info.empty();
+		$(this).val() ? showEstimation($(this)) : false;
 		$(this).prop('disabled', false);
 		$(this).focus();
 	});
-	//$expenseValue.keydown(amountEvents);
+	
+	$expenseValue.keydown(function() {
+		($(this).val().length>10) ? $(this).val($(this).val().slice(0, -1)) : false;
+	});
 	
 	$category.on("change", function() {
 		$checked = $(this);
@@ -117,5 +168,8 @@ $(document).ready(function() {
 	$dateInput.on("change", function() {
 		showEstimation($expenseValue);
 	});
+	
+	updateInfo($expenseValue, $checked)
+	
 });
 
