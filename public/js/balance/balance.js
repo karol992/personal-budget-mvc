8f879f4b5303868f3b9ac03c00d268473da575dd
Karol Sołek <karol.solek.programista@gmail.com>
2019-10-11 02:33:42 +0200

    Balance View(js): load expense in modal'

--- public/js/balance/balance.js
+++ public/js/balance/balance.js
@@ -6,6 +6,8 @@ $inc_id_label=$('#incomeModalIdLabel');
 $inc_list=$('#incomeCategoryList');
 
 $exp_label=$('#expenseModalLabel');
+$exp_id_label=$('#expenseModalIdLabel');
+$exp_list=$('#expenseCategoryList');
 
 function editIncomeRecord() {
 	$form = $(this).closest('form');
@@ -15,7 +17,6 @@ function editIncomeRecord() {
 		dataType : "json",
 		data: $form.serialize()
 	}).done(function(response) {
-		console.log(response.new_sum);
 		$form.next('div.error').empty().attr('hidden', true);
 		if(!response.success) {
 			$form.next('div.error').attr('hidden', false);
@@ -28,14 +29,15 @@ function editIncomeRecord() {
 	}).fail(function() {
 		alert("income edit fail");
 	});
-	
 }
+
+function editExpenseRecord() {}
 
 function loadIncomeRecord(id, amount, date, comment) { $inc_list.append($('<li class="container">').append(
 	$('<form class="modal_line row shadow">').append(
 		$('<input type="hidden" name="income_id" value="" />').attr('value', id),
 		$('<input type="hidden" name="category_id" value="" />').attr('value', $inc_id_label.val()),
-		$('<input type="text" name="amount" class="modal_cell col-6 col-sm-6 col-lg-2" step="0.01" value="" min="0.01">').attr('value', amount),
+		$('<input type="number" name="amount" class="modal_cell col-6 col-sm-6 col-lg-2" step="0.01" value="" min="0.01">').attr('value', amount),
 		$('<input type="date" name="income_date" class="modal_cell col-6 col-sm-6 col-lg-3" value="">').attr('value', date),		
 		$('<input type="text" name="comment" class="modal_cell col-8 col-sm-9 col-lg-5" value="">').attr('value', comment).attr('placeholder','Notatki...'),	
 		$('<div class="container modal_cell col-4 col-sm-3 col-lg-2" style="padding: 0;">').append(
@@ -43,6 +45,39 @@ function loadIncomeRecord(id, amount, date, comment) { $inc_list.append($('<li c
 			$('<button type="button" class="btn_record bg_del modal_button delete_record_button">').html('<i class="fa fa-trash fa-fw"></i>'))),
 	$('<div class="error bot10" hidden>')
 ))};
+
+function loadExpenseRecord(id, amount, date, comment) { $exp_list.append($('<li class="container">').append(
+	$('<form class="modal_line row shadow">').append(
+		$('<input type="hidden" name="expense_id" value="" />').attr('value', id),
+		$('<input type="hidden" name="category_id" value="" />').attr('value', $inc_id_label.val()),
+		$('<input type="number" name="amount" class="modal_cell col-12 col-sm-4 col-lg-2" step="0.01" value="" min="0.01">').attr('value', amount),
+		$('<input type="date" name="expense_date" class="modal_cell col-6 col-sm-4 col-lg-3" value="">').attr('value', date),
+		$('<select name="payment" class="modal_cell col-6 col-sm-4 col-lg-2">'),		
+		$('<input type="text" name="comment" class="modal_cell col-8 col-lg-3" value="">').attr('value', comment).attr('placeholder','Notatki...'),	
+		$('<div class="container modal_cell col-4 col-lg-2" style="padding: 0;">').append(
+			$('<button type="button" class="btn_record modal_button edit_record_button">').on('click', editExpenseRecord).html('<i class="fa fa-floppy-o fa-fw"></i>'),
+			$('<button type="button" class="btn_record bg_del modal_button delete_record_button">').html('<i class="fa fa-trash fa-fw"></i>'))),
+	$('<div class="error bot10" hidden>')
+))};
+/*
+<li class="container">
+								<form class="modal_line row shadow">
+										<input type="hidden" name="expense_id" value="" />
+										<input type="number" name="amount" class="modal_cell col-12 col-sm-4 col-lg-2" step="0.01" value="" min="0.01" />
+										<input type="date" name="expense_date" class="modal_cell col-6 col-sm-4 col-lg-3" value="" />
+										<select name="payment" class="modal_cell col-6 col-sm-4 col-lg-2">
+											{% for pay in payment_cats%}<option value="{{ pay.id }}">{{ pay.name }}</option>{% endfor %}
+										</select>
+										<input type="text" name="comment" class="modal_cell col-8 col-lg-3" placeholder="Notatki..." onfocus="this.placeholder=''Notatki..." onblur="this.placeholder='Notatki...'" value="" />
+										<div class="container modal_cell col-4 col-lg-2" style="padding: 0;">
+											<button name="action" value="update" type="submit" class="btn_record modal_button edit_record_button"><i class="fa fa-floppy-o fa-fw"></i></button> 
+											<button name="action" value="delete" type="submit" class="btn_record bg_del modal_button delete_record_button"><i class="fa fa-trash fa-fw"></i></button>
+										</div>
+								</form>
+						</li>*/
+
+
+
 
 $inc_btn.on('click' , function(e) {
 	e.preventDefault();
